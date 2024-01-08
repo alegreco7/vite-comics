7c9d1a6e9b870bc51445f4482b3da86be4022de7
alegreco7 <alexgreco2002@gmail.com>
2024-01-08 15:45:33 +0100

    add header with style

---
+++ src/components/App.Header.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+
+}
+</script>
+<template lang="">
+    <header>
+        <div class="logo">
+            <img src="../assets/img/dc-logo.png" alt="logo">
+        </div>
+        <div class="nav-list">
+
+        </div>
+    </header>
+</template>
+<style lang="scss" scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 0;
+
+    .logo img {
+        width: 50px;
+    }
+
+    .nav-list {
+        width: 900px;
+        height: 50px;
+        background-color: aqua;
+    }
+}
+</style>
