af0ade56b4cb6e9e4fbad0f502cb0cf243a2d4ff
alegreco7 <alexgreco2002@gmail.com>
2024-01-09 16:09:15 +0100

    add new component AppComic to AppMain

---
+++ src/components/AppComic.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    props: {
+        comic_obj: Object
+    }
+}
+</script>
+<template lang="">
+    <div class="comic">
+        <img :src="comic_obj.thumb" :alt="comic_obj.series">
+        <h3>{{ comic_obj.series }}</h3>
+    </div>
+</template>
+<style lang="scss" scoped>
+@use '../styles/generals.scss';
+@use '../styles/partials/variables' as *;
+
+.comic {
+    margin: 10px;
+    width: calc(100% / 6 - 20px);
+
+    img {
+        width: 100%;
+        height: 200px;
+    }
+
+    h3 {
+        color: $white;
+    }
+}</style>
